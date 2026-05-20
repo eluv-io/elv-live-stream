66ba68ed104e61615480bd7d2cb60223bc63e6e2
Zenia Villa <zenia.villa@eluv.io>
2026-05-20 09:17:45 -0700

    Exclude css from test coverage

--- vitest.config.js
+++ vitest.config.js
@@ -1,21 +1,37 @@
-import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react-swc';
-import path from 'path';
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import {fileURLToPath} from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.js',
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.js",
     onConsoleLog(log) {
       if(log.includes("non-boolean attribute `inert`")) { return false; }
       if(log.includes("React Router Future Flag Warning")) { return false; }
     },
   },
+  coverage: {
+    provider: "v8",
+    reporter: ["text", "json", "html"],
+    include: ["src/**/*"],
+    exclude: [
+      "**/*.module.css",
+      "src/assets/**/*",
+      "**/constants.js",
+      "src/test/setup.js",
+      "**/*.test.{js,jsx,ts,tsx}"
+    ]
+  },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
