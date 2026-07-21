a59f7fe00a9010e13057554ebac44098de91a14d
Zenia Villa <zenia.villa@eluv.io>
2026-07-21 13:27:30 -0700

    Fix alert message color

--- src/components/alert-message/AlertMessage.jsx
+++ src/components/alert-message/AlertMessage.jsx
@@ -18,7 +18,7 @@ const AlertMessage = ({error, onClose, ...props}) => {
     <Box ref={errorRef} mb={16} {...props}>
       <Alert
         variant="light"
-        color="red"
+        color="elv-red.1"
         title={title}
         withCloseButton
         onClose={onClose}
